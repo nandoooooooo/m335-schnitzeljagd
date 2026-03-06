6b27a6fa85ecbd6884cf0a6b3ee7a0244e4c991d
Nando Schuermann <nando.schuermann@getabstract.com>
2026-03-06 15:20:22 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'm335-schnitzeljagd',
+  webDir: 'www'
+};
+
+export default config;
