fdd13b4b4614216fa5bef68b16a1f7dfda1c75d0
T-Fux <Timo_Fuchs@sluz.ch>
2026-03-10 11:36:04 +0100

    - permissions-page added and html skeleton made.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,5 +9,9 @@ export const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },  {
+    path: 'permissions-page',
+    loadComponent: () => import('./permissions-page/permissions-page.page').then( m => m.PermissionsPagePage)
   },
+
 ];
